4f7e1901c29aef47f940adf6aad0249c0a08763e
Sreenivas V Rao <sreenivasvrao1@gmail.com>
2018-10-05 12:33:38 -0400

    Use torch DataLoader.

--- code/SuperSloMo/utils/adobe_240fps.py
+++ code/SuperSloMo/utils/adobe_240fps.py
@@ -3,22 +3,29 @@ import cv2
 import numpy as np
 import random, os
 from math import ceil
-
+import math
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 
 log = logging.getLogger(__name__)
 
 
-class Reader:
+class Reader(DataLoader):
 
-    def __init__(self, cfg, split="TRAIN"):
+    def __init__(self, cfg, split="TRAIN", transform=None):
+
         self.cfg = cfg
-        self.batch_size = self.cfg.getint("TRAIN", "BATCH_SIZE")
         self.compute_scale_factors()
         self.clips = self.read_clip_list(split)
         self.split = split
+        self.transform = transform
+
         log.info(split+ ": Extracted clip list.")
 
+
     def read_clip_list(self, split):
+
         fpath = self.cfg.get("ADOBE_DATA", split+"PATHS")
         with open(fpath, "rb") as f:
             data = f.readlines()
@@ -40,6 +47,7 @@ class Reader:
         return clips
 
     def compute_scale_factors(self):
+
         self.H = self.cfg.getint("ADOBE_DATA", "H")
         self.W = self.cfg.getint("ADOBE_DATA", "W")
         self.dims = (self.H, self.W)
@@ -54,63 +62,127 @@ class Reader:
 
         self.scale_factors= (self.s_y, self.s_x)
 
-    def get_clips(self):
-        """
-        Generator that can yield clips from video, each clip is of size n_frames.
+    def __len__(self):
 
-        numpy array, BGR format. uint8.
+        return len(self.clips)
 
-        :param video_path: full path to video
-        :param n_frames: number of frames to extract for each clip
-        :return:
-        """
+    def __getitem__(self, idx):
 
-        log.info(self.split+ ": Running generator for extracting clips.")
-        random.shuffle(self.clips)
-        frame_buffer = np.zeros([self.batch_size, 9, self.H, self.W, 3])
+        img_paths = self.clips[idx]
+        H = 720 # known size of Adobe240FPS dataset
+        W = 1280 # known size of Adobe dataset.
+        frames = np.zeros([len(img_paths), H, W, 3])
+        for idx, fpath in enumerate(img_paths):
+            img = cv2.imread(fpath)
+            h, w, c = img.shape
+            if (h,w) == (720, 1280):
+                frames[idx,...] = img
+            elif (h,w) == (1280, 720):
+                frames[idx, ...] = img.swapaxes(0, 1) # flipping the image for vertical videos
+            else:
+                log.info(str(img.shape) + " "+fpath)
 
-        count = 0
+        if self.transform:
+            frames = self.transform(frames)
 
-        for idx, clip in enumerate(self.clips):
-            if len(clip) <9:
-                continue
-            elif len(clip) < 12:
-                start_idx = random.randint(0, len(clip) - 9)
-                # random starting point to get 9 frames.
-                img_paths = clip[start_idx: start_idx + 9]
-            elif len(clip) >= 12:
-                subset = random.randint(0, len(clip)-12)
-                # random starting point to get subset of 12 frames.
-                clip_12 = clip[subset:subset+12]
-                start_idx = random.randint(0, 3) # 12 - 9
-                # random starting point to get subset of 9 frames.
-                img_paths = clip_12[start_idx: start_idx +9]
+        return frames
 
-            images = [cv2.imread(fpath) for fpath in img_paths]
-            images = [cv2.resize(image, (640, 360)) for image in images]
-            images = np.array(images)
+class ResizeCrop(object):
+    """
+    Convert 720 x 1280 frames to 320 x 640 -> Resize + Random Cropping
+    """
 
-            h_start = random.randint(0, 360-self.H) # random height crop.
-            images = images[:, h_start:h_start+self.H, ...]
-            frame_buffer[count, ...] = images
+    def __call__(self, sample_frames):
 
-            count+=1
-            if count == self.batch_size:
-                yield frame_buffer
-                frame_buffer = np.zeros([self.batch_size, 9, self.H, self.W, 3])
-                count = 0
+        new_frames = np.zeros((sample_frames.shape[0], 360, 640, 3))
+        for idx in range(sample_frames.shape[0]):
+            new_frames[idx, ...] = cv2.resize(sample_frames[idx, ...], (640, 360))
 
-        if count>0: # last remaining batch
-            yield frame_buffer[:count, ...]
+        h_start  =random.randint(0, 360-320)
+
+        new_frames = new_frames[:, h_start:h_start+320, ...]
+        return new_frames
+
+class ToTensor(object):
+
+    def __call__(self, sample):
+        sample = torch.from_numpy(sample)
+        sample = sample.permute(0, 3, 2, 1) # n_frames, H W C -> n_frames, C, H, W
+        return sample
+
+
+def collate_fn(data):
+    """
+    Custom collate function.
+    :params data: list of tensors to collate. each of size [n_frames, C, H, W]
+    :return batch_data: batch of size B, 9, C, H, W
+    """
+
+    for idx in range(len(data)):
+        tensor_data = data[idx]
+        assert len(tensor_data.shape)==4, "Shape: "+str(tensor_data.shape)
+        n_frames = tensor_data.shape[0]
+        if n_frames == 9:
+            continue
+
+        elif n_frames < 12:
+            start_idx = random.randint(0, n_frames - 9) # random starting point to get 9 frames.
+            clip = tensor_data[start_idx: start_idx + 9, ...]
+            data[idx] = clip
+
+        elif n_frames >= 12:
+            subset = random.randint(0, n_frames-12) # random starting point to get subset of 12 frames.
+            clip_12 = tensor_data[subset:subset+12, ...]
+
+            start_idx = random.randint(0, 3) # random starting point to get subset of 9 frames.
+            clip = clip_12[start_idx:start_idx+9, ...]
+            data[idx] = clip
+
+    batch = torch.stack(data)
+    return batch
+
 
 
 
 if __name__ == '__main__':
     import ConfigParser
+    import logging
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("--log")
+    parser.add_argument("--config") # config
+    args = parser.parse_args()
+
+
+    logging.basicConfig(filename=args.log, level=logging.INFO)
+
     config = ConfigParser.RawConfigParser()
-    config.read("../../config.ini")
-    adobe_dataset = Reader(config, split="TRAIN")
-    for aClip in adobe_dataset.get_clips():
-        np.save("Clips.npy", aClip)
-        log.info(""+str(aClip.shape))
-        exit(0)
+    config.read(args.config)
+    logging.info("Read config")
+
+    transformations = transforms.Compose([ResizeCrop(), ToTensor()])
+
+    batch_size = config.getint("TRAIN", "BATCH_SIZE")
+
+    adobe_dataset = Reader(config, split="TRAIN", transform=transformations)
+
+    import time
+
+    adobe_loader = DataLoader(adobe_dataset, batch_size=batch_size, shuffle=True, num_workers=16, collate_fn=collate_fn)
+
+    start = time.time()
+
+    for idx, batch_sample in enumerate(adobe_loader):
+        log.info(str(idx))
+        if idx==19:
+            break
+
+
+    log.info(str(batch_sample.shape)+" shape of input tensor.")
+
+    stop = time.time()
+    total = (stop - start)
+    log.info(stop-start)
+
+    log.info(str(float(total)/20.0))
