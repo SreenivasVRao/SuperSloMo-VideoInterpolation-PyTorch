Data augmentation. Quick fix to loss computation on multiple GPUs.

code/SuperSloMo/models/SSMLoss.py
@@ -34,7 +34,7 @@ class PerceptualLoss(nn.Module):
             param.requires_grad = False
         for param in self.parameters():
             param.requires_grad = False
-        self.modulelist = list(self.vgg16.features.modules())[1:23] # until conv4_3
+        self.modulelist = list(self.vgg16.features.modules())[1:24] # until relu(conv4_3)
 
     def rescale(self, tensor):
         """
@@ -55,12 +55,11 @@ class PerceptualLoss(nn.Module):
         return tensor_normed
 
     def forward(self, x_input, x_target):
-        x_input = self.rescale(x_input)
-        x_target = self.rescale(x_target)
+        # x_input = self.rescale(x_input)
+        # x_target = self.rescale(x_target)
         for aLayer in self.modulelist: # until conv4_3
             x_input = aLayer(x_input)
             x_target = aLayer(x_target)
-
         perceptual_loss = self.l2_loss(x_input, x_target)
 
         return perceptual_loss
@@ -208,7 +207,15 @@ class SSMLosses(nn.Module):
         loss_perceptual =lambda_p * self.get_perceptual_loss(interpolated_image*255.0, target_image*255.0)
         loss_warp = lambda_w * self.get_warp_loss(flowC_input, flowC_output, flowI_input, flowI_output, target_image)*255.0
 
+        batch_size = flowC_input.shape[0]
+
         total_loss =  loss_reconstr +  loss_warp +  loss_perceptual
+        total_loss = total_loss/4.0
+        
+        """
+        Loss is averaged on each GPU. So combining across 4 GPUs, we need to average it.
+        """
+        
         loss_list = [total_loss, loss_reconstr, loss_warp, loss_perceptual]
 
         loss_tensor = torch.stack(loss_list).squeeze()

code/SuperSloMo/utils/adobe_240fps.py
@@ -8,6 +8,7 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
+
 log = logging.getLogger(__name__)
 
 
@@ -33,7 +34,7 @@ class Reader(DataLoader):
         with open(fpath, "rb") as f:
             data = f.readlines()
             data = [d.strip() for d in data]
-
+            
         clips = []
 
         data = [d.replace("/home/", "/mnt/nfs/work1/elm/") for d in data]
@@ -70,8 +71,6 @@ class Reader(DataLoader):
 
         return image_list
 
-
-
     def compute_scale_factors(self):
 
         self.H = self.cfg.getint("ADOBE_DATA", "H")
@@ -107,13 +106,44 @@ class Reader(DataLoader):
         if h==1280 and w==720: # vertical video. W = 720, H =1280
             frames = frames.swapaxes(1, 2)
 
-        if np.random.randint(0, 2)==1:
-            frames = frames[:,:,::-1,:] # horizontal flip 50% of the time
+        if self.split=="TRAIN":
+            frames = self.augment_data(frames)
 
         if self.transform:
             frames = self.transform(frames)
 
         return frames
+
+    def augment_data(self, frames):
+        """
+        Flips the images horizontally 50% of the time.
+        Performs a random affine transform of the data.
+        """
+        if np.random.randint(0, 2)==1:
+            frames = frames[:,:,::-1,:] # horizontal flip 50% of the time
+
+        tx = np.random.randint(-25, 25)
+        ty = np.random.randint(-25, 25)
+        theta = np.random.uniform(-np.pi/30, np.pi/30)
+
+        N, H, W, C = frames.shape
+
+        M  = np.array([[1, 0, 0], [0, 1, 0]]).astype(np.float32)
+
+        M[0, 2] = tx
+        M[1, 2] = ty
+        M[0, 0] = M[1, 1] = np.cos(theta)
+        M[0, 1] = np.sin(theta)
+        M[1, 0] = -np.sin(theta)
+
+        for idx in range(N):
+            img = frames[idx,...]
+            frames[idx,...] = cv2.warpAffine(img, M, (W, H))
+
+        return frames
+    
+
+        
 
 class ResizeCrop(object):
     """
@@ -139,7 +169,7 @@ class ToTensor(object):
     Converts np 0-255 uint8 to 0 -1 tensor
     """
     def __call__(self, sample):
-        sample = torch.from_numpy(sample)/255.0
+        sample = torch.from_numpy(sample.copy())/255.0
         sample = sample.permute(0, 3, 1, 2) # n_frames, H W C -> n_frames, C, H, W
         return sample
 

code/main.py
@@ -68,8 +68,6 @@ class SSM_Main:
         :param split: Train/Val,
         :return:
         """
-
-
         total_loss, loss_reconstr,  loss_warp,loss_perceptual = losses
 
         self.writer.add_scalars('Total_Loss', {split: total_loss.item()}, iteration)
@@ -87,14 +85,14 @@ class SSM_Main:
         :return: if get_interpolation is set, returns interpolation result as BCHW Variable.
         otherwise returns the losses.
         """
+        
         data_batch = data_batch.cuda().float()
         img_0 = data_batch[:, 0, ...]
         img_t = data_batch[:, 1, ...]
-        img_1 = data_batch[:, 2, ...]
+        img_1 = data_batch[:, -1, ...]
 
         if not get_interpolation:
             loss_buffer = torch.autograd.Variable(torch.from_numpy(np.zeros([1, 4]))).float().cuda(1)
-
             losses = self.superslomo(img_0, img_1, dataset_info, self.t_interp, img_t, loss_buffer, split, iteration)[0,:]
             self.write_losses(losses, iteration, split)
             total_loss = losses[0]
@@ -124,7 +122,6 @@ class SSM_Main:
 
             for train_batch in adobe_train_samples:
                 iteration +=1
-
                 train_loss = self.forward_pass(train_batch, train_info, "TRAIN", iteration)
 
                 optimizer.zero_grad()
@@ -171,27 +168,30 @@ class SSM_Main:
         total_PSNR = 0
 
         nframes = 0
+
         for iteration, a_batch in enumerate(dataset):
-            est_image_t, gt_image_t = self.forward_pass(a_batch, info, split, iteration, get_interpolation=True)
-            est_image_t = est_image_t * 255.0
-            gt_image_t  = gt_image_t * 255.0
+            a_batch = a_batch.cuda().float()
+            for idx in range(1, 8):
+                t_interp = float(idx)/8
+                est_image_t, _  = self.forward_pass(a_batch, info, split, iteration, t_interp, get_interpolation=True)
+                gt_image_t = a_batch[:, idx, ...]
+                est_image_t = est_image_t * 255.0
+                gt_image_t  = gt_image_t * 255.0
 
-            est_image_t = est_image_t.permute(0, 2, 3, 1) # BCHW -> BHWC
-            gt_image_t  = gt_image_t.permute(0, 2, 3, 1) # BCHW -> BHWC
+                est_image_t = est_image_t.permute(0, 2, 3, 1) # BCHW -> BHWC
+                gt_image_t  = gt_image_t.permute(0, 2, 3, 1) # BCHW -> BHWC
 
-            est_image_t = est_image_t.cpu().data.numpy()
-            gt_image_t  =  gt_image_t.cpu().data.numpy()
+                est_image_t = est_image_t.cpu().data.numpy()
+                gt_image_t  =  gt_image_t.cpu().data.numpy()
 
-            est_image_t = est_image_t.astype(np.uint8)
-            gt_image_t  =  gt_image_t.astype(np.uint8)
-
-            IE_scores = metrics.interpolation_error(est_image_t, gt_image_t)
-            ssim_scores = metrics.ssim(est_image_t, gt_image_t)
-            psnr_scores = metrics.psnr(est_image_t, gt_image_t)
-
-            total_IE   += np.sum(IE_scores)
-            total_ssim += np.sum(ssim_scores)
-            total_PSNR += np.sum(psnr_scores)
+                est_image_t = est_image_t.astype(np.uint8)
+                gt_image_t  =  gt_image_t.astype(np.uint8)
+                IE_scores = metrics.interpolation_error(est_image_t, gt_image_t)
+                ssim_scores = metrics.ssim(est_image_t, gt_image_t)
+                psnr_scores = metrics.psnr(est_image_t, gt_image_t)
+                total_IE   += np.sum(IE_scores)
+                total_ssim += np.sum(ssim_scores)
+                total_PSNR += np.sum(psnr_scores)
             n_interpolations = a_batch.shape[1]-2 # exclude i_0, i_1
             nframes += a_batch.shape[0]*n_interpolations  # interpolates nframes Batch size - 2 frames (i0, i1)
 
@@ -233,21 +233,21 @@ if __name__ == '__main__':
     model = SSM_Main(cfg, args.expt, args.msg)
 
 
-    # model.train()
+    model.train()
 
-    model.superslomo.eval()
+    # model.superslomo.eval()
 
-    adobe_train = adobe_240fps.data_generator(cfg, split="TRAIN")
-    adobe_val = adobe_240fps.data_generator(cfg, split="VAL")
-    train_info = adobe_240fps.get_data_info(cfg, split="TRAIN")
-    val_info = adobe_240fps.get_data_info(cfg, split="VAL")
+    # adobe_train = adobe_240fps.data_generator(cfg, split="TRAIN")
+    # adobe_val = adobe_240fps.data_generator(cfg, split="VAL")
+    # train_info = adobe_240fps.get_data_info(cfg, split="TRAIN")
+    # val_info = adobe_240fps.get_data_info(cfg, split="VAL")
 
 
-    PSNR, IE, SSIM = model.compute_metrics(adobe_train, train_info, "TRAIN")
-    logging.info("ADOBE TRAIN: Average PSNR %.3f IE %.3f SSIM %.3f"%(PSNR, IE, SSIM))
+    # PSNR, IE, SSIM = model.compute_metrics(adobe_train, train_info, "TRAIN")
+    # logging.info("ADOBE TRAIN: Average PSNR %.3f IE %.3f SSIM %.3f"%(PSNR, IE, SSIM))
 
-    PSNR, IE, SSIM = model.compute_metrics(adobe_val, val_info, "VAL")
-    logging.info("ADOBE VAL: Average PSNR %.3f IE %.3f SSIM %.3f"%(PSNR, IE, SSIM))
+    # PSNR, IE, SSIM = model.compute_metrics(adobe_val, val_info, "VAL")
+    # logging.info("ADOBE VAL: Average PSNR %.3f IE %.3f SSIM %.3f"%(PSNR, IE, SSIM))
 
     # PSNR, IE, SSIM = model.compute_metrics(adobe_test)
     # logging.info("ADOBE TEST: PSNR ", PSNR, " IE: ", IE, " SSIM: ", SSIM)